0107f4d5beb52c053a1ca53757e60c0e27020d96
amadeus <amadeusdemarzi@gmail.com>
2011-01-12 11:32:25 -0800

    * errm

--- Source/Counter.js
+++ Source/Counter.js
@@ -175,6 +175,7 @@ var Counter = this.Counter = new Class({
 		var toAdd = [],
 			clean = function(el, i){
 				if (i % 2 === 1) return toAdd.push(el);
+				console.log(el);
 				el.destroy();
 			},
 			insert = function(el, i){
